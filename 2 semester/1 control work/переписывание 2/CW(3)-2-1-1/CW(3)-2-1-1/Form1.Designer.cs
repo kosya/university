c94679ea8e151c8e5ea9ef9e4b5f1e28f95f4b69
Александр Косицын <i.s.a.n.y.a@mail.ru>
2013-05-07 15:20:29 +0400

    Второе переписывание контрольной

---
+++ 2 semester/1 control work/переписывание 2/CW(3)-2-1-1/CW(3)-2-1-1/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CW_3__2_1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Click ME";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 2 semester/1 control work/переписывание 2/CW(3)-2-1-1/CW(3)-2-1-1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_3__2_1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Method, which changes button position
+        /// </summary>
+        public void Changer()
+        {
+            Random Rand = new Random();
+            button1.Location = new Point(Rand.Next(0, this.Size.Width - 2 * this.button1.Size.Width), Rand.Next(0, this.Size.Height - 2 * this.button1.Size.Height));
+        }
+
+        /// <summary>
+        /// Event of button click, which close the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Event which changes button position, when cursor is entering button area
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Changer();
+        }
+
+        /// <summary>
+        /// Event which changes button position, when cursor is in button area for a some time
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            Changer();
+        }
+    }
+}
